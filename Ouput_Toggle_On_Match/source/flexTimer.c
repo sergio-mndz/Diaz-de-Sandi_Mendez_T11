755de23a781e51aa7b586d7c1cfd9f3bf01e0e10
Sergio Mendez <39496193+sergio-mndz@users.noreply.github.com>
2020-03-19 10:24:29 -0600

    Working first implementation

--- Ouput_Toggle_On_Match/source/flexTimer.c
+++ Ouput_Toggle_On_Match/source/flexTimer.c
@@ -28,11 +28,11 @@ void FlexTimer_Init()
 	/**Selects the FTM behavior in BDM mode.In this case in functional mode*/
 	FTM0->CONF |= FTM_CONF_BDMMODE(3);
 	/**Assign modulo register with a predefined value*/
-	FTM0->MOD = 0x05;
+	FTM0->MOD = 0x09;
 	/**Configure FlexTimer in output compare in toggle mode*/
 	FTM0->CONTROLS[0].CnSC = FTM_CnSC_MSA(1) | FTM_CnSC_ELSA(1);
 	/**Assign channel value register with a predefined value*/
-	FTM0->CONTROLS[0].CnV = 0x03;
+	FTM0->CONTROLS[0].CnV = 0x01;
 	/**Select clock source and prescaler*/
-	FTM0->SC |= FTM_SC_CLKS (FLEX_TIMER_CLKS_1)| FTM_SC_PS(FLEX_TIMER_PS_128);
+	FTM0->SC |= FTM_SC_CLKS (FLEX_TIMER_CLKS_1)| FTM_SC_PS(FLEX_TIMER_PS_1);
 }
