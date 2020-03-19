b350a05106b60897f99af7a641fb393b191e11aa
Sergio Mendez <39496193+sergio-mndz@users.noreply.github.com>
2020-03-18 22:14:58 -0600

    Changes

--- Ouput_Toggle_On_Match/source/main.c
+++ Ouput_Toggle_On_Match/source/main.c
@@ -33,38 +33,16 @@
  * @brief   Application entry point.
  */
 #include <stdio.h>
-#include "board.h"
-#include "peripherals.h"
-#include "pin_mux.h"
-#include "clock_config.h"
+#include <stdint.h>
 #include "MK64F12.h"
-#include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
-
-/* TODO: insert other definitions and declarations here. */
+#include "flexTimer.h"
+#include "bits.h"
+#include "GPIO.h"
 
 /*
  * @brief   Application entry point.
  */
 int main(void) {
 
-  	/* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
-  	/* Init FSL debug console. */
-    BOARD_InitDebugConsole();
 
-    PRINTF("Hello World\n");
-
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
-    return 0 ;
 }
