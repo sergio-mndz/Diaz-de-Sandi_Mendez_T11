9863c1fc3b5e12facd9dcf05c6d588fa854c5211
Kevin Diaz de Sandi <ie715630@iteso.mx>
2020-03-18 21:27:36 -0600

    Variable PWM first commit

---
+++ implementacion_4/FlexTimer.h
@@ -0,0 +1,47 @@
+/**
+	\file
+	\brief
+		This is the header file for the FlexTimer divice driver.
+	\author J. Luis Pizano Escalante, luispizano@iteso.mx
+	\date	21/03/2019
+	\todo
+	    Add configuration structures.
+ */
+
+#ifndef FLEXTIMER_H_
+#define FLEXTIMER_H_
+
+
+#include "MK64F12.h"
+
+#define FLEX_TIMER_CLKS_0  (0U)
+#define FLEX_TIMER_CLKS_1  (1U)
+#define FLEX_TIMER_CLKS_2  (2U)
+#define FLEX_TIMER_CLKS_3  (3U)
+
+
+#define FLEX_TIMER_PS_1    (0U)
+#define FLEX_TIMER_PS_2    (1U)
+#define FLEX_TIMER_PS_4    (2U)
+#define FLEX_TIMER_PS_8    (3U)
+#define FLEX_TIMER_PS_16    (4U)
+#define FLEX_TIMER_PS_32    (5U)
+#define FLEX_TIMER_PS_64    (6U)
+#define FLEX_TIMER_PS_128    (7U)
+
+
+#define  FLEX_TIMER_DMA   0x01
+#define  FLEX_TIMER_ELSA  0x04
+#define  FLEX_TIMER_ELSB  0x08
+#define  FLEX_TIMER_MSA   0x10
+#define  FLEX_TIMER_MSB   0x20
+#define  FLEX_TIMER_CHIE  0x40
+#define  FLEX_TIMER_CHF   0x80
+
+
+void FlexTimer_update_channel_value(int16_t channel_value);
+
+void FlexTimer_Init();
+
+
+#endif /* FLEXTIMER_H_ */
---
+++ implementacion_4/main.c
@@ -0,0 +1,72 @@
+/**
+	\file
+	\brief
+		This project shows how to configure FlexTimer in PWM mode.
+	\author J. Luis Pizano Escalante, luispizano@iteso.mx
+	\date	21/03/2019
+	\todo
+	    Add configuration structures.
+ */
+
+
+#include "MK64F12.h" /* include peripheral declarations */
+#include "GPIO.h"
+#include "FlexTimer.h"
+#include "NVIC.h"
+#include "stdint.h"
+#include "Bits.h"
+
+uint16_t g_duty_cycle;
+
+void PORTA_handler(void)
+{
+	g_duty_cycle += 38;
+
+	if(g_duty_cycle < 0xFF)
+	{
+		FlexTimer_update_channel_value(g_duty_cycle);
+	}
+
+	GPIO_clear_interrupt(GPIO_C);
+}
+
+void PORTC_handler(void)
+{
+	g_duty_cycle -= 12;
+
+	if(g_duty_cycle > 0)
+	{
+		FlexTimer_update_channel_value(g_duty_cycle);
+	}
+
+	GPIO_clear_interrupt(GPIO_A);
+}
+
+int main(void)
+{
+	gpio_pin_control_register_t pcr_in = GPIO_MUX1 | GPIO_PE | GPIO_PS | INTR_FALLING_EDGE ;
+	gpio_pin_control_register_t alt4 = GPIO_MUX4;
+	GPIO_clock_gating(GPIO_A);
+	GPIO_clock_gating(GPIO_C);
+	GPIO_pin_control_register(GPIO_C, bit_6, &pcr_in);
+	GPIO_pin_control_register(GPIO_A, bit_4, &pcr_in);
+	GPIO_pin_control_register(GPIO_C, bit_1, &alt4);
+	GPIO_data_direction_pin(GPIO_C, GPIO_INPUT, bit_6);
+	GPIO_data_direction_pin(GPIO_A, GPIO_INPUT, bit_4);
+	GPIO_data_direction_pin(GPIO_C, GPIO_OUTPUT, bit_1);
+	NVIC_enable_interrupt_and_priotity(PORTC_IRQ, PRIORITY_5);
+	NVIC_enable_interrupt_and_priotity(PORTA_IRQ, PRIORITY_5);
+	GPIOC_callback_init(PORTC_handler);
+	GPIOA_callback_init(PORTA_handler);
+	NVIC_global_enable_interrupts;
+	FlexTimer_Init();
+
+	g_duty_cycle = 204;
+	printf("%d\n", g_duty_cycle);
+	while(1)
+	{
+
+	}
+
+	return 0;
+}
