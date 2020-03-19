2705401a5ddd32f3b15f386924feb210731d7fbc
Sergio Mendez <39496193+sergio-mndz@users.noreply.github.com>
2020-03-18 23:11:31 -0600

    First test

--- Output_Compare_Clear_On_Match/source/flexTimer.c
+++ Output_Compare_Clear_On_Match/source/flexTimer.c
@@ -1,91 +1,39 @@
-/*
- * flexTimer.c
- *
- *  Created on: 14 mar. 2020
- *      Author: kcdia
+/**
+	\file
+	\brief
+		This is the starter file of FlexTimer.
+		In this file the FlexTimer is configured in overflow mode.
+	\author J. Luis Pizano Escalante, luispizano@iteso.mx
+	\date	21/03/2019
+	\todo
+	    Add configuration structures.
  */
 
-#include "flexTimer.h"
 
-#define IC_FREC (21000000.0 / (128.0 + 1.0))
 
-void FTM0_IRQHandler()
-{
-	static uint32_t last_value = 0;
-	static uint32_t current_value = 0;
-	int32_t diff = 0;
-	uint32_t time = 0;
+#include "FlexTimer.h"
+#include "MK64F12.h"
 
-	last_value = current_value;
-	current_value = FTM0->CONTROLS[0].CnV;
-
-	diff = current_value - last_value;
-	if(diff < 0)
-	{
-		diff += 0xFFFF;
-	}
-	time = IC_FREC / diff;
-
-	FTM0->CONTROLS[0].CnSC &= ~(FTM_CnSC_CHF_MASK);
-}
-
-void FTM_init_input_capture(FlexTimer_configType * FTM_conf)
-{
-	FTM_clk(FTM_conf->FTM);
-	FTM_MOD(FTM_conf->FTM, FTM_conf->MODE);
-	FTM_CONF(FTM_conf->FTM, FTM_conf->CONF);
-	FTM_MOD(FTM_conf->FTM, FTM_conf->MOD);
-	FTM_CnSC(FTM_conf->FTM, FTM_conf->CH, FTM_conf->CnSC);
-	FTM_SC(FTM_conf->FTM, FTM_conf->SC);
-}
-
-void FTM_clk(FTM_Type * FTM)
+void FTM0_ISR()
 {
 
-	if(FTM == FTM0)
-	{
-		SIM->SCGC6 |= SIM_SCGC6_FTM0(1);
-	}
-	else if(FTM == FTM1)
-	{
-		SIM->SCGC6 |= SIM_SCGC6_FTM1(1);
-	}
-	else if(FTM == FTM2)
-	{
-		SIM->SCGC3 |= SIM_SCGC3_FTM2(1);
-	}
-	else
-	{
-		SIM->SCGC3 |= SIM_SCGC3_FTM3(1);
-	}
 }
 
-void FTM_MODE(FTM_Type * FTM, uint32_t mode)
+void FlexTimer_Init()
 {
-	FTM->MODE |= mode;
+	/**Clock gating for FlexTimer*/
+	SIM->SCGC6 |= SIM_SCGC6_FTM0(1);
+	/**It enable the FTM*/
+	FTM0->MODE |= FTM_MODE_FTMEN_MASK;
+	/**Selects the FTM behavior in BDM mode.In this case in functional mode*/
+	FTM0->CONF |= FTM_CONF_BDMMODE(3);
+	/**Assign modulo register with a predefined value*/
+	FTM0->MOD = 0x05;
+	/**Configure FlexTimer in output compare in toggle mode*/
+	FTM0->CONTROLS[0].CnSC = FTM_CnSC_MSA(1) | FTM_CnSC_ELSA(2);
+	/**Assign channel value register with a predefined value*/
+	FTM0->CONTROLS[0].CnV = 0x03;
+	/**Select clock source and prescaler*/
+	FTM0->SC |= FTM_SC_CLKS (FLEX_TIMER_CLKS_1)| FTM_SC_PS(FLEX_TIMER_PS_128);
 }
 
-void FTM_CONF(FTM_Type * FTM, uint32_t conf)
-{
-	FTM->CONF |= conf;
-}
-
-void FTM_MOD(FTM_Type * FTM, uint32_t mod)
-{
-	FTM->MOD |= mod;
-}
-
-void FTM_CnSC(FTM_Type * FTM, FTMchannel ch, uint32_t csc)
-{
-	FTM->CONTROLS[ch].CnSC |= csc;
-}
-
-void FTM_SC(FTM_Type * FTM, uint32_t sc)
-{
-	FTM->SC |= sc;
-}
-
-void FTM_CnV(FTM_Type * FTM, FTMchannel ch, uint32_t count_value)
-{
-	FTM->CONTROLS[ch].CnV |= count_value;
-}
--- Output_Compare_Clear_On_Match/source/flexTimer.h
+++ Output_Compare_Clear_On_Match/source/flexTimer.h
@@ -1,21 +1,24 @@
-/*
- * flexTimer.h
- *
- *  Created on: 14 mar. 2020
- *      Author: kcdia
+/**
+	\file
+	\brief
+		This is the header file for the FlexTimer divice driver.
+	\author J. Luis Pizano Escalante, luispizano@iteso.mx
+	\date	21/03/2019
+	\todo
+	    Add configuration structures.
  */
 
 #ifndef FLEXTIMER_H_
 #define FLEXTIMER_H_
 
+
 #include "MK64F12.h"
-#include "stdint.h"
-#include "stdio.h"
 
 #define FLEX_TIMER_CLKS_0  (0U)
 #define FLEX_TIMER_CLKS_1  (1U)
 #define FLEX_TIMER_CLKS_2  (2U)
 #define FLEX_TIMER_CLKS_3  (3U)
+
 
 #define FLEX_TIMER_PS_1    (0U)
 #define FLEX_TIMER_PS_2    (1U)
@@ -26,31 +29,7 @@
 #define FLEX_TIMER_PS_64    (6U)
 #define FLEX_TIMER_PS_128    (7U)
 
-typedef enum {CH_0, CH_1, CH_2, CH_3, CH_4, CH_5, CH_6, CH_7, CH_8} FTMchannel;
-
-typedef struct
-{
-	FTM_Type * FTM;
-	FTMchannel CH;
-	uint32_t MODE;
-	uint32_t CONF;
-	uint32_t MOD;
-	uint32_t CNTIN;
-	uint32_t CnSC;
-	uint32_t SC;
-	uint32_t CnV;
-} FlexTimer_configType;
-
-void FTM_init_input_capture(FlexTimer_configType * FTM_conf);
-
-void FTM_clk(FTM_Type * FTM);
-void FTM_MODE(FTM_Type * FTM, uint32_t mode);
-void FTM_CONF(FTM_Type * FTM, uint32_t conf);
-void FTM_MOD(FTM_Type * FTM, uint32_t mod);
-void FTM_CnSC(FTM_Type * FTM, FTMchannel ch, uint32_t csc);
-void FTM_SC(FTM_Type * FTM, uint32_t sc);
-void FTM_CnV(FTM_Type * FTM, FTMchannel ch, uint32_t count_value);
-
+void FlexTimer_Init();
 
 
 #endif /* FLEXTIMER_H_ */
